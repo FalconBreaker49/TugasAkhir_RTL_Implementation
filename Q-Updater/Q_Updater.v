aa46b6af46ea711805676a789d1a56cd3e1e941d
Jalu Reswara <99407314+FalconBreaker49@users.noreply.github.com>
2023-04-14 07:38:05 +0700

    Update Q_Updater.v
    Mengupdate Q-updater untuk menerima dua Q-values dari A dan B

--- Q-Updater/Q_Updater.v
+++ Q-Updater/Q_Updater.v
@@ -8,15 +8,22 @@ module QA(
     input wire [31:0] Q0_A, Q1_A, Q2_A, Q3_A,
     input wire [31:0] Q0_B, Q1_B, Q2_B, Q3_B,
     input wire [31:0] R,
-    input wire [1:0] A, Amax,
+    input wire [1:0] A_A, Amax_A, A_B, Amax_B,
     input wire [2:0] alpha, gamma,
-    output reg [31:0] Qnew
+    output reg [31:0] Qnew_A,Qnew_B
     );
     
+    // Reward R for both agents are the same. Action (A) and Action Max (Amax) are separate for both agents. 
+    
     // REGISTERS
-    reg [31:0] Q0_reg0, Q1_reg0, Q2_reg0, Q3_reg0;
-    reg [31:0] Q0_reg1, Q1_reg1, Q2_reg1, Q3_reg1;
-    reg [1:0] Amax_reg0, A_reg0;
+    //For Intersection A (Intersection 1)
+    reg [31:0] Q0_reg0_A, Q1_reg0_A, Q2_reg0_A, Q3_reg0_A;
+    reg [31:0] Q0_reg1_A, Q1_reg1_A, Q2_reg1_A, Q3_reg1_A;
+    reg [1:0] Amax_reg0_A, A_reg0_A;
+    //For Intersection B (Intersection 2)
+    reg [31:0] Q0_reg0_B, Q1_reg0_B, Q2_reg0_B, Q3_reg0_B;
+    reg [31:0] Q0_reg1_B, Q1_reg1_B, Q2_reg1_B, Q3_reg1_B;
+    reg [1:0] Amax_reg0_B, A_reg0_B;
 //    always @(posedge clk) begin 
 //        if (rst) begin
 //            Q0_reg0 <= 32'd0;
@@ -48,30 +55,46 @@ module QA(
 //        end
 //    end
     always @(posedge clk) begin 
-        // Q-VALUES
-        Q0_reg0 <= Q0;
-        Q1_reg0 <= Q1;
-        Q2_reg0 <= Q2;
-        Q3_reg0 <= Q3;
-        Q0_reg1 <= Q0_reg0;
-        Q1_reg1 <= Q1_reg0;
-        Q2_reg1 <= Q2_reg0;
-        Q3_reg1 <= Q3_reg0;
-        // ACTIONS
-        Amax_reg0 <= Amax;
-        A_reg0 <= A;
-        // REWARD
-        R_reg0 <= R;
+        // Q-VALUES for Intersection A
+        Q0_reg0_A <= Q0_A;
+        Q1_reg0_A <= Q1_A;
+        Q2_reg0_A <= Q2_A;
+        Q3_reg0_A <= Q3_A;
+        Q0_reg1_A <= Q0_reg0_A;
+        Q1_reg1_A <= Q1_reg0_A;
+        Q2_reg1_A <= Q2_reg0_A;
+        Q3_reg1_A <= Q3_reg0_A;
+        // ACTIONS for Intersection A
+        Amax_reg0_A <= Amax_A;
+        A_reg0_A <= A_A;
+        // REWARD for Intersection A
+        R_reg0_A <= R;
+        
+        // Q-VALUES for Intersection B
+        Q0_reg0_B <= Q0_B;
+        Q1_reg0_B <= Q1_B;
+        Q2_reg0_B <= Q2_B;
+        Q3_reg0_B <= Q3_B;
+        Q0_reg1_B <= Q0_reg0_B;
+        Q1_reg1_B <= Q1_reg0_B;
+        Q2_reg1_B <= Q2_reg0_B;
+        Q3_reg1_B <= Q3_reg0_B;
+        // ACTIONS for Intersection B
+        Amax_reg0_B <= Amax_B;
+        A_reg0_B <= A_B;
+        // REWARD for Intersection B
+        R_reg0_B <= R;
+        
     end
-
-    wire [31:0] Qmax;
-    mux4to1_32bit max0( .in0(Q0_reg0),   .in1(Q1_reg0),   .in2(Q2_reg0),   .in3(Q3_reg0),
-                        .sel(Amax_reg0),  .out0(Qmax)
+    //For maximum Q-value Intersection A 
+    wire [31:0] Qmax_A;
+    mux4to1_32bit max0( .in0(Q0_reg0_A),   .in1(Q1_reg0_A),   .in2(Q2_reg0_A),   .in3(Q3_reg0_A),
+                       .sel(Amax_reg0_A),  .out0(Qmax_A)
                         );
     
-    wire [31:0] Qsel;
-    mux4to1_32bit mux0( .in0(Q0_reg1),   .in1(Q1_reg1),   .in2(Q2_reg1),   .in3(Q3_reg1),
-                        .sel(A_reg0),    .out0(Qsel)
+    wire [31:0] Qsel_A;
+    mux4to1_32bit mux0( .in0(Q0_reg1_A),   .in1(Q1_reg1_A),   .in2(Q2_reg1_A),   .in3(Q3_reg1_A),
+                       .sel(A_reg0_A),    .out0(Qsel_A)
                         );
     
     // REGISTER SELECTED Q-VALUES                    
