8374cd4b0856d366ce7ed9b52b8b749539529f73
Jalu Reswara <99407314+FalconBreaker49@users.noreply.github.com>
2023-04-18 15:30:58 +0700

    Update Reward_Decider.v
    Creating a new version of the Reward based on previous work where there are 3 rewards given based on which action is appropriate. Max Action corresponds to the max-Q-value and Min Action corresponds to min-Q-value.

--- Reward/Reward_Decider.v
+++ Reward/Reward_Decider.v
@@ -7,25 +7,35 @@ module RD(
     // ------------
     input wire clk, rst,
     input wire [31:0] R0, R1, R2,
-    input wire [1:0] Amax, Amin, A,
-    output reg [31:0] R        
+    input wire [1:0] Amax_A, Amin_A, A_A,
+    input wire [1:0] Amax_B, Amin_B, A_B,
+    output reg [31:0] R_A,R_B        
     );
       
-    reg [1:0] Amax_reg0, Amin_reg0;
+    reg [1:0] Amax_reg0_A, Amin_reg0_A;
+    reg [1:0] Amax_reg0_B, Amin_reg0_B;
     always @(posedge clk) begin
-        Amax_reg0 <= Amax;
-        Amin_reg0 <= Amin;
+        Amax_reg0_A <= Amax_A;
+        Amin_reg0_A <= Amin_A;
+        
+        Amax_reg0_B <= Amax_B;
+        Amin_reg0_B <= Amin_B;
     end
     
-    wire [31:0] Rtemp;
-    assign Rtemp = (A==Amax_reg0)? R2 : 
-                   ((A==Amin_reg0)? R0 : R1);
+    wire [31:0] Rtemp_A, Rtemp_B;
+    assign Rtemp_A = (A_A==Amax_reg0_A)? R2 : 
+                     ((A_A==Amin_reg0_A)? R0 : R1);
     
-    reg [31:0] R_reg0;
+    
+    assign Rtemp_B = (A_B==Amax_reg0_B)? R2 : 
+                     ((A_B==Amin_reg0_B)? R0 : R1);
+    
+    reg [31:0] R_reg0_A, R_reg0_B;
     always @(posedge clk) begin
-        R_reg0 <= Rtemp;
-        R <= R_reg0;
-        
+        R_reg0_A <= Rtemp_A;
+        R_reg0_B <= Rtemp_B;
+        R_A <= R_reg0_A;
+        R_B <= R_reg0_B;
     end
     
 //    wire [31:0] R_reg0;          
