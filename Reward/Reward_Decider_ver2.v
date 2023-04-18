d04767021562246121872d3e7ab7140921a5ccf8
Jalu Reswara <99407314+FalconBreaker49@users.noreply.github.com>
2023-04-18 15:57:27 +0700

    Update Reward_Decider_ver2.v
    Membuat landasan penjelasan untuk Reward Decider ver 2. RD ini memberikan + Reward kepada Agent ketika lane tersebut kosong (berhasil dikosongkan setelah dibeirkan action) atau yang bernilai 0. Memberikan Reward (-) jika ada mobil (lane tersebut tidak kosong).

--- Reward/Reward_Decider_ver2.v
+++ Reward/Reward_Decider_ver2.v
@@ -2,4 +2,17 @@
 // Inti dari pemberian rewardnya adalah dilihat setelah memberikan action ke current state, di state tersebut 
 // Adalah lanes yang level kmeacetannya bernilai 0 atau tidak. Jika ada yang bernilai 0, dibeirkan + reward
 // Lane yang tidak nol menjadi negative reward. 
-
+// Cuma ada dua reward. Soalnya dia cuma ngeliat ada yang 0 -> positive reward kalau gak 0 --> negative reward. 
+// Outputnya 1 saja karena reward untuk A dan B digabung jadi 1.
+// Dari module State Decider, S_A dan S_B ukurannya 12 bit. 3 bit masing-masing untuk setiap lane (4 lane)
+module RD (
+  //----------
+  input wire clk, rst, 
+  input wire [31:0] R0, R1,
+  input wire [11:0] S_A, S_B,
+  output reg  [31:0] R0,
+  output wire [31:0] R
+);
+  
+// Flownya adalah dari State_A dan State_B --> Partisi jadi Lane_0 sampai Lane_3 --> Bandingkan setiap lane berapa banyak 1 atau 0 nya
+// Setiap kali ada 0 --> +Reward . Setiap kali ada 1 --> -Reward. 
