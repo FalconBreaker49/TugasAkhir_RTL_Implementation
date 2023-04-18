9516ec7eea0d1e5adda652fe52037a8158fa9e0a
Jalu Reswara <99407314+FalconBreaker49@users.noreply.github.com>
2023-04-18 14:34:47 +0700

    Update State_Converter.v
    Mengganti port-port Input dan Output

--- States_Related/State_Converter.v
+++ States_Related/State_Converter.v
@@ -2,16 +2,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 // States converter to simulate the traffic environment
 //////////////////////////////////////////////////////////////////////////////////
-
+// A_A : Action A, A_B : Action B
+// S0_A & S0_B : Intial state untuk A dan B??
+// traffic_A dan B itu real traffic in terms of vehicles kan?
+// S_A dan S_B adalah converted traffic states to state
 module SD(
     input wire clk, rst, learning,
-    input wire [1:0] A,
-    input wire [11:0] S0,
-    input wire [11:0] traffic,
-    output wire [11:0] S,
+    input wire [1:0] A_A,A_B,
+    input wire [11:0] S0_A, S0_B,
+    input wire [11:0] traffic_A,traffic_B,
+    output wire [11:0] S_A,S_B
     // for debugging 
-    output wire [2:0] level0, level1, level2, level3,
-    output reg [2:0] L0, L1, L2, L3
+    output wire [2:0] level0_A, level1_A, level2_A, level3_A,
+    output wire [2:0] level0_B, level1_B, level2_B, level3_B,
+    output reg [2:0] L0_A, L1_A, L2_A, L3_A,
+    output reg [2:0] L0_B, L1_B, L2_B, L3_B
     );
        
 //    reg [2:0] L0, L1, L2, L3;
