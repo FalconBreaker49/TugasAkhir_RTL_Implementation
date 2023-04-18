2540b5bb775f0ba6e74bf789f20ff66275842ba4
Jalu Reswara <99407314+FalconBreaker49@users.noreply.github.com>
2023-04-18 15:32:46 +0700

    Create Reward_Decider_ver2.v

---
+++ Reward/Reward_Decider_ver2.v
@@ -0,0 +1,5 @@
+// Ini adalah Reward Decider version 2 menggunakan algoritma tersendiri 
+// Inti dari pemberian rewardnya adalah dilihat setelah memberikan action ke current state, di state tersebut 
+// Adalah lanes yang level kmeacetannya bernilai 0 atau tidak. Jika ada yang bernilai 0, dibeirkan + reward
+// Lane yang tidak nol menjadi negative reward. 
+
